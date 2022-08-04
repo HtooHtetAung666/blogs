d7b52bacfc11c6b3f15100e4368277c8575b121e
ryzen666 <htoohtetaung26@gmail.com>
2022-08-04 21:46:58 +0630

    fetch data with custom hook

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    let [blogs,setBlogs] = useState([
-        {title:'Blog 1',body:'Blog body one',author:'blog a1',id:1},
-        {title:'Blog 2',body:'Blog body two',author:'blog a2',id:2},
-        {title:'Blog 3',body:'Blog body three',author:'blog a3',id:3},
-    ])
+
+    const {data: blogs, pending, error} = useFetch('http://localhost:8000/blogs');
+
     return ( 
         <div className="home">
-           <BlogList blogs={blogs} title='All Blogs!'/>
+            {error && <p>{error}</p>}
+            {pending && <p>Loading ....</p>}
+            {blogs && <BlogList blogs={blogs} title='All Blogs!'/>}
         </div>
      );
 }
